c9c97c49edcd06a7af5133a8eccd9417b3f55f37
maany <imptodefeat@gmail.com>
2021-05-11 13:50:13 +0200

    build pipeline

--- build_py.sh
+++ build_py.sh
@@ -1,4 +1,23 @@
 #/bin/bash
-protoc -I=./proto/ --python_out=./prada-py ./proto/*.proto
-cp -rf prada-py prada/
-touch prada/prada-py/__init__.py
+# protoc -I=./proto/ --python_out=./prada-py ./proto/*.proto
+# cp -rf prada-py prada/
+# touch prada/prada-py/__init__.py
+
+echo "Activating VirtualEnv"
+cd prada
+source ./.virtualenv/bin/activate
+
+echo "Installing grpcio-tools"
+pip install grpcio-tools
+
+echo "Creating interface package for gRPC code"
+
+mkdir -p interface/service interface/model
+touch interface/__init__.py interface/service/__init__.py interface/model/__init__.py
+
+echo "Building gRPC services"
+python -m grpc_tools.protoc -I=../proto --python_out=./interface/model --grpc_python_out=./interface/service ../proto/*.proto
+
+echo "Deactivating Virtualenv"
+deactivate
+cd ../
